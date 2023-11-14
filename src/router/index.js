88ffe7a63637b98ffd813d8b5c48d67a1656f320
mvazquez <matias.vazquezmartin@gmail.com>
2023-11-14 15:23:10 -0300

    feat: index router

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,10 @@ const crudController = require("../controller/crud.controller");
 const youtubeController = require("../controller/youtube.controller");
 const contactController = require("../controller/message.controller");
 const githubController = require("../controller/github.controller");
+const indexController = require("../controller/index.controller");
 
 const router = (app) => {
+  app.use("/", indexController);
   app.use("/chatlogs", chatController);
   app.use("/cruds", crudController);
   app.use("/getinfochannels", youtubeController);
