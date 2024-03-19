2fc616cd629263c9b198f935ba8d8dbbf6dac9cb
mvazquez <matias.vazquezmartin@gmail.com>
2024-03-19 13:49:40 -0300

    fix: dirname at express.static

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("src/public"));
+app.use(express.static(__dirname + "/public"));
 
 router(app);
 
