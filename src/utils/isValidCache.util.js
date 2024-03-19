c5f4cf06bab95851d7ea59a05b74efd3e82774fc
mvazquez <matias.vazquezmartin@gmail.com>
2024-03-19 20:43:04 -0300

    fix: deploy vercel

--- src/utils/isValidCache.util.js
+++ src/utils/isValidCache.util.js
@@ -1,31 +1,31 @@
 //DEPRECADO POR CLASS CACHEMANAGER
 
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 
-const cacheFilePath = path.join(
-  __dirname,
-  "../dao/apiYoutubeCache/file/cacheYoutube.json"
-);
+// const cacheFilePath = path.join(
+//   __dirname,
+//   "../dao/apiYoutubeCache/file/cacheYoutube.json"
+// );
 
-const cacheData = require(cacheFilePath);
+// const cacheData = require(cacheFilePath);
 
-const isValidCache = (channelId) => {
-  try {
-    if (!fs.existsSync(cacheFilePath)) return false;
-    if (!cacheData[channelId]) return false;
+// const isValidCache = (channelId) => {
+//   try {
+//     if (!fs.existsSync(cacheFilePath)) return false;
+//     if (!cacheData[channelId]) return false;
 
-    const currentTime = new Date().getTime();
-    const lastUpdatedTime = cacheData[channelId].timestamp;
-    const fileAge = currentTime - lastUpdatedTime;
+//     const currentTime = new Date().getTime();
+//     const lastUpdatedTime = cacheData[channelId].timestamp;
+//     const fileAge = currentTime - lastUpdatedTime;
 
-    // const cacheExpiration = 24 * 60 * 60 * 1000;
-    const cacheExpiration = 5 * 60 * 1000;
+//     // const cacheExpiration = 24 * 60 * 60 * 1000;
+//     const cacheExpiration = 5 * 60 * 1000;
 
-    return fileAge <= cacheExpiration;
-  } catch (error) {
-    throw error;
-  }
-};
+//     return fileAge <= cacheExpiration;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-module.exports = isValidCache;
+// module.exports = isValidCache;
