abfc2f14a28566d96444629852736188890ebd23
mvazquez <matias.vazquezmartin@gmail.com>
2024-04-23 09:43:08 -0300

    fix: minor changes

--- src/public/index.js
+++ src/public/index.js
@@ -23,7 +23,6 @@ const account = document.getElementById("account");
 const btnNavBarAside = document.querySelectorAll(".iconNavBarAside");
 
 // FUNCIONES
-//
 
 // EVENTLISTENERS
 explorer.addEventListener("click", renderExplorer);
