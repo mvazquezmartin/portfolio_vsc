02ec67c5453473b851b32a11715e8e52cb4da38e
mvazquez <matias.vazquezmartin@gmail.com>
2024-04-30 14:08:27 -0300

    refactor: explorer render function

--- src/public/js/components/explorerSideBar/explorer.js
+++ src/public/js/components/explorerSideBar/explorer.js
@@ -15,59 +15,41 @@ import {
 } from './folderFileObj.js';
 import { folderFileSideBar } from './folderFileRender.js';
 
-const renderExplorer = () => {
-  const titleSideBar = document.getElementById('titleSideBar');
-  titleSideBar.textContent = 'EXPLORER';
+const folderArray = [
+  assetsFolder,
+  followMe,
+  guessTheColor,
+  guessTheNumber,
+  magic8Ball,
+  pongWarsFolder,
+  playGroundFolder,
+  chat,
+  crud,
+  dotenv,
+  gitIgnore,
+  packageJson,
+  readme,
+];
 
-  const viewSideBar = document.getElementById('viewSideBar');
-  viewSideBar.innerHTML = '';
+const renderExplorer = () => {
+  const $titleSideBar = document.getElementById('titleSideBar');
+  $titleSideBar.textContent = 'EXPLORER';
+
+  const $viewSideBar = document.getElementById('viewSideBar');
+  $viewSideBar.innerHTML = '';
 
   const fileOpen = document.getElementById('fileOpen');
   fileOpen.textContent = 'PORTFOLIO';
 
   //Carpetas en Explorer
-  const assets = folderFileSideBar(assetsFolder);
-  viewSideBar.appendChild(assets);
-
-  const followMeGame = folderFileSideBar(followMe);
-  viewSideBar.appendChild(followMeGame);
-
-  const guessTheColorGame = folderFileSideBar(guessTheColor);
-  viewSideBar.appendChild(guessTheColorGame);
-
-  const guessTheNumberGame = folderFileSideBar(guessTheNumber);
-  viewSideBar.appendChild(guessTheNumberGame);
-
-  const magic8ball = folderFileSideBar(magic8Ball);
-  viewSideBar.appendChild(magic8ball);
-
-  const playGround = folderFileSideBar(playGroundFolder);
-  viewSideBar.appendChild(playGround);
-
-  const pongWars = folderFileSideBar(pongWarsFolder);
-  viewSideBar.appendChild(pongWars);
-
-  const chatSocket = folderFileSideBar(chat);
-  viewSideBar.appendChild(chatSocket);
-
-  const crudFolder = folderFileSideBar(crud);
-  viewSideBar.appendChild(crudFolder);
-
-  const env = folderFileSideBar(dotenv);
-  viewSideBar.appendChild(env);
-
-  const ignore = folderFileSideBar(gitIgnore);
-  viewSideBar.appendChild(ignore);
-
-  const json = folderFileSideBar(packageJson);
-  viewSideBar.appendChild(json);
-
-  const readmeMain = folderFileSideBar(readme);
-  viewSideBar.appendChild(readmeMain);
+  folderArray.forEach((folder) => {
+    const newFolder = folderFileSideBar(folder);
+    $viewSideBar.appendChild(newFolder);
+  });
 
   const collapseFolder = document.createElement('i');
   collapseFolder.classList.add('bi', 'bi-chevron-bar-contract');
-  
+
   const $allFolder = document.querySelectorAll('.panel');
   collapseFolder.addEventListener('click', () => {
     $allFolder.forEach((folder) => (folder.style.maxHeight = null));
