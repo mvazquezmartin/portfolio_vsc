2d2eee75a59a809526924d015596e6d607abf943
mvazquez <matias.vazquezmartin@gmail.com>
2024-08-26 16:01:06 -0300

    fix: darkmode sweetalert

--- src/public/js/components/chat/utils/sweetAlert.js
+++ src/public/js/components/chat/utils/sweetAlert.js
@@ -1,14 +1,15 @@
 const alertNewUser = async () => {
   return await Swal.fire({
-    title: "Log In",
-    input: "text",
-    text: "Ingresa un usuario para chatear",
-    color: "#fff",
+    title: 'Log In',
+    input: 'text',
+    text: 'Ingresa un usuario para chatear',
+    color: '#fff',
     confirmButtonText: 'Connect',
     showCancelButton: true,
-    cancelButtonColor: "#d33",
+    cancelButtonColor: '#d33',
+    background: '#282C34',
     inputValidator: (value) => {
-      return !value && "Necesitas ingresar un usuario para continuar";
+      return !value && 'Necesitas ingresar un usuario para continuar';
     },
 
     allowOutsideClick: false,
@@ -18,14 +19,15 @@ const alertNewUser = async () => {
 
 const alertConnection = (user, status, icon) => {
   Swal.fire({
-    text: `Se acaba de ${status} ${user}`,
-    color: "#fff",
+    text: `Se acaba de ${status}: ${user}`,
+    color: '#fff',
     toast: true,
-    position: "top-right",
+    position: 'top-right',
     showConfirmButton: false,
     timer: 3000,
     timerProgressBar: true,
     icon: icon,
+    background: '#282C34',
   });
 };
 
